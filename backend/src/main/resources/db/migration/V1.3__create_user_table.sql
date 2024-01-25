6e3b8083c3cee55a79ada38cf9c655a6c9334f8f
Denis Magda <dmagda@gridgain.com>
2024-01-25 16:01:45 -0500

    new password

--- backend/src/main/resources/db/migration/V1.3__create_user_table.sql
+++ backend/src/main/resources/db/migration/V1.3__create_user_table.sql
@@ -9,8 +9,8 @@ CREATE TABLE user_account(
 );
 
 INSERT INTO user_account(id, email, PASSWORD, full_name, user_location)
-    VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'user1@gmail.com', '$2a$10$0nwRIPBdOVLrcwnb0WDm6.1k0vOSbkRfnTNVlOJdRqOKgG5qWe.lK', 'John Doe', 'New York'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'user2@gmail.com', '$2a$10$0nwRIPBdOVLrcwnb0WDm6.1k0vOSbkRfnTNVlOJdRqOKgG5qWe.lK', 'Emely Smith', 'San Francisco'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'user3@gmail.com', '$2a$10$0nwRIPBdOVLrcwnb0WDm6.1k0vOSbkRfnTNVlOJdRqOKgG5qWe.lK', 'Michael Williams', 'Los Angeles'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'user4@gmail.com', '$2a$10$0nwRIPBdOVLrcwnb0WDm6.1k0vOSbkRfnTNVlOJdRqOKgG5qWe.lK', 'Jessica Brown', 'Boston');
+    VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'user1@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'John Doe', 'New York'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'user2@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Emely Smith', 'San Francisco'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'user3@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Michael Williams', 'Los Angeles'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'user4@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Jessica Brown', 'Boston');
 
