396b56ad9ace5e676a70652d5c8256824143369c
Denis Magda <dmagda@gridgain.com>
2024-02-05 16:08:56 -0500

    default user for api calls

--- backend/src/main/java/com/yugaplus/backend/controller/UserLibraryController.java
+++ backend/src/main/java/com/yugaplus/backend/controller/UserLibraryController.java
@@ -1,6 +1,8 @@
 package com.yugaplus.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -44,14 +46,23 @@ public class UserLibraryController {
 
     @PutMapping("/add/{movieId}")
     public UserLibraryResponse addMovieToLibrary(@PathVariable Integer movieId) {
-        UserRecord authUser = SecurityConfig.getAuthenticatedUser().get();
+        Optional<UserRecord> authUser = SecurityConfig.getAuthenticatedUser();
+
+        UUID userId;
+
+        // FOR TESTING PURPOSES
+        if (!authUser.isEmpty()) {
+            userId = authUser.get().getUserId();
+        } else {
+            userId = UUID.fromString("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+        }
 
         try {
             long startTime = System.currentTimeMillis();
 
             jdbcClient.sql("""
                     INSERT INTO user_library (user_id, movie_id) VALUES (?, ?)
-                    """).params(authUser.getUserId(), movieId).update();
+                    """).params(userId, movieId).update();
 
             long execTime = System.currentTimeMillis() - startTime;
 
@@ -64,12 +75,21 @@ public class UserLibraryController {
 
     @DeleteMapping("/remove/{movieId}")
     public UserLibraryResponse removeMovieFromLibrary(@PathVariable Integer movieId) {
-        UserRecord authUser = SecurityConfig.getAuthenticatedUser().get();
+        Optional<UserRecord> authUser = SecurityConfig.getAuthenticatedUser();
+
+        UUID userId;
+
+        // FOR TESTING PURPOSES
+        if (!authUser.isEmpty()) {
+            userId = authUser.get().getUserId();
+        } else {
+            userId = UUID.fromString("a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11");
+        }
 
         try {
             jdbcClient.sql("""
                     DELETE FROM user_library WHERE user_id = ? AND movie_id = ?
-                    """).params(authUser.getUserId(), movieId).update();
+                    """).params(userId, movieId).update();
             return new UserLibraryResponse(new Status(true, HttpServletResponse.SC_OK), null);
         } catch (Exception e) {
             return new UserLibraryResponse(new Status(true, HttpServletResponse.SC_INTERNAL_SERVER_ERROR), null);
